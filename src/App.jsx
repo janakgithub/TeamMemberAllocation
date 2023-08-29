63bc91537047f8772de0be69f9fff3c876db7ee3
janakadhikari2 <25873191-janakadhikari2@users.noreply.replit.com>
2023-08-29 10:08:01 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+import Employees from './Employees'
+
+export default function App() {
+  return (
+    <main>
+      <Header/>
+      <Employees/>
+      <Footer/>
+    </main>
+  )
+}
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return(
+  <header>
+    <h1 className="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3">Header</h1>
+  </header>
+    )
+}
+
+export default Header
